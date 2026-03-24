20ac0ae943ab67fe5b00c8cfa41a8bd669faa4c3
vathsala333 <vathsala455@gmail.com>
2026-03-24 18:28:58 +0530

    Removed unused services

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors({
 app.use(express.json());
 
 // Routes
-app.use("/api/auth", authRoutes);
+//app.use("/api/auth", authRoutes);
 app.use("/api/tickets", ticketRoutes);
 app.use("/api/ai", aiRoutes); // ✅ ADD THIS
 
